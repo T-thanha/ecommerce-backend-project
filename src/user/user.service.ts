365cc0134170f79f84888e66b92901340273ead1
Thanakrit <thanakrit@MacBook-Air-khxng-Thanakrit.local>
2025-03-21 21:47:12 +0700

    Add bcrypt for password hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,20 +4,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt'
+
 @Injectable()
 export class UserService {
-  constructor(    
-    @InjectRepository(User)
-    private userRepository: Repository<User>){}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
   async create(createUserDto: CreateUserDto) {
-    const User = createUserDto;
     try {
-      const is_created = await this.userRepository.save(User)
+      const User = createUserDto;
+      User.password = await bcrypt.hash(User.password, 10);
+      const is_created = await this.userRepository.save(User);
       if (is_created) {
         throw new HttpException('User created successfully', 200);
       }
-    }
-    catch (error) {
+    } catch (error) {
       throw new HttpException(error, 500);
     }
   }
